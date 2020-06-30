52ae7f479da3244624a705b71e1a7d4c334a228e
Chris <eltrov@gmail.com>
2020-06-29 23:47:14 -0500

    Update HarryPotterWiki.user.js
    I can't words

--- HarryPotterWiki.user.js
+++ HarryPotterWiki.user.js
@@ -2,7 +2,7 @@
 // @name         Un-Fuck the Harry Potter Wiki
 // @namespace    https://eltrov.com
 // @version      0.3
-// @description  The Harry Potter Wiki (located at https://harrypotter.fandom.com) is an abysmal website bogged down with and obscene amount of garbage. This script removes that garbge and delivers are pure wiki reading experience.
+// @description  The Harry Potter Wiki (located at https://harrypotter.fandom.com) is an abysmal website bogged down with and obscene amount of garbage. This script removes that garbge and delivers a more pure wiki reading experience.
 // @author       eltrov
 // @match        https://harrypotter.fandom.com/*
 // @grant        none
