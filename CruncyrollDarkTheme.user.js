19aaa9ac86d414e68640ade293fb55d7fb12c373
Chris <eltrov@gmail.com>
2020-07-01 18:56:28 -0500

    adding include

--- CruncyrollDarkTheme.user.js
+++ CruncyrollDarkTheme.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Crunchyroll Background Color Changer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.11
 // @description  Gives Cruncyroll a dark theme using my preferred dark-mode hex color code (#222426)
 // @author       eltrov
 // @grant        none
+// @include      https://www.crunchyroll.com/*
 // @downloadURL  https://raw.githubusercontent.com/eltrov/TampermonkeyStuff/master/CruncyrollDarkTheme.user.js
 // ==/UserScript==
 
@@ -13,3 +14,7 @@ document.body.setAttribute("style", "background-color: #222426;");
 
 // I haven't done any extensive testing on this but so far this single line has been all I've needed.
 // I'll revisit this if the site necessitates more 'fixes' like this
+
+
+// To-Do:
+// text needs to be changed to be more visible on pages. This script was originally only intended to be used for the viewing pages but can (I think) easily be expanded
