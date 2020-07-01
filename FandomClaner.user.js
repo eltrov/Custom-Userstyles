6081f06ae6390577daa368415fd7d47a5f0c106d
Chris <eltrov@gmail.com>
2020-07-01 14:22:53 -0500

    Experimental General-Use Fandom Cleaner
    I've copied over the Harry Potter wiki cleaner I made and broadened the scope to apply to any fandom wiki sites. This is experimental so use at your own discretion.

---
+++ FandomClaner.user.js
@@ -0,0 +1,78 @@
+// ==UserScript==
+// @name         Fandom Cleaner
+// @namespace    https://github.com/eltrov/TampermonkeyStuff
+// @version      0.1
+// @description  Greatly reduces the amout of visual clutter on Fandom wiki sites
+// @author       eltrov
+// @match        https://*.fandom.com/*
+// @grant        none
+// @noframes
+// @downloadURL  https://raw.githubusercontent.com/eltrov/TampermonkeyStuff/master/FandomClaner.user.js
+// ==/UserScript==
+
+function GM_addStyle(css) {
+  const style = document.getElementById("GM_addStyleBy8626") || (function() {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = "GM_addStyleBy8626";
+    document.head.appendChild(style);
+    return style;
+  })();
+  const sheet = style.sheet;
+  sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+}
+
+// wrapper left-side image
+//GM_addStyle('.body.background-dynamic.skin-oasis::after, body.background-dynamic.skin-oasis::before { display:none;}');
+GM_addStyle('.body.background-dynamic.skin-oasis::after, body.background-dynamic.skin-oasis::before { background-color:#222426; background-image: none;}');
+
+// wrapper right-side image
+GM_addStyle('.body.background-dynamic.skin-oasis::before, body.background-dynamic.skin-oasis::after { background-color:#222426; background-image: none;}');
+
+// body background color of the outside of wrapper (outer reaches)
+GM_addStyle('body.background-dynamic.skin-oasis { background-color:#222426;}');
+
+// erase Wikia Rail - this single element was what made me create this script in the first place
+GM_addStyle('.WikiaRail { display:none; }');
+
+// adjust main content width to fully fill the new area
+GM_addStyle('.WikiaMainContent { width:100%; }');
+
+// wrapper right-side image
+GM_addStyle('.WikiaTopAds { display: none;}');
+
+// wiki page body
+GM_addStyle('.WikiaPage { width: 80%;}');
+
+//  community banner
+GM_addStyle('.wds-community-header { width: auto;}');
+
+// removes ALL content below the body of the wiki page except comments where applicable
+GM_addStyle('.WikiaFooter { display: none;}');
+
+// below is no longer needed (child element of the above footer)
+GM_addStyle('.mcf-en { display: none;}');
+
+// Fandom company footer, including corporate links
+GM_addStyle('.wds-global-footer { display: none;}');
+
+// Removed the wrapper that runs across the very bottom of the page, linking to random other things (ads?)
+GM_addStyle('.WikiaBarWrapper { display: none;}');
+
+// Fandom company links (to other topics/wikis) - this essentially slims down the header bar to just the search bar itself
+GM_addStyle('.wds-global-navigation__content-bar-left { display: none;}');
+
+// Removed the "Start a Wiki" page and fully right-justifys the search bar
+GM_addStyle('.wds-global-navigation__start-a-wiki { display: none;}');
+
+// Removes the Community Header bar with links to the various books and other common pages. Also unfortunately removes the "random page" button. Link: https://*.fandom.com/wiki/Special:Random
+GM_addStyle('.wds-community-header { display: none;}');
+
+//template for extra elements if more need to be added
+GM_addStyle('.WikiaBar { display: none;}');
+
+// Removes the login icon to the right of the searhc bar
+GM_addStyle('.wds-global-navigation__link-group, .wds-global-navigation__notifications-dropdown, .wds-global-navigation__user-menu { display: none;}');
+
+//template for extra elements if more need to be added
+//GM_addStyle('.foo { display: none;}');
